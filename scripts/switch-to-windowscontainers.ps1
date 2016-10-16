fedccc8d4680b12384c499d31e192c0b6c9f0e52
Naeem Sarfraz <naeem.sarfraz@gmail.com>
2016-10-16 01:54:49 +0100

    fixing checkin oops, shouldn't have commented out lines to download

--- scripts/switch-to-windowscontainers.ps1
+++ scripts/switch-to-windowscontainers.ps1
@@ -1,6 +1,6 @@
-#Invoke-WebRequest "https://master.dockerproject.org/windows/amd64/docker-1.13.0-dev.zip" -OutFile "$env:TEMP\docker-1.13.0-dev.zip" -UseBasicParsing
+Invoke-WebRequest "https://master.dockerproject.org/windows/amd64/docker-1.13.0-dev.zip" -OutFile "$env:TEMP\docker-1.13.0-dev.zip" -UseBasicParsing
 Expand-Archive -Path "$env:TEMP\docker-1.13.0-dev.zip" -DestinationPath $env:ProgramFiles -Force
 $env:path += ";c:\program files\docker"
-#[Environment]::SetEnvironmentVariable("Path", $env:Path + ";C:\Program Files\Docker", [EnvironmentVariableTarget]::Machine)
+[Environment]::SetEnvironmentVariable("Path", $env:Path + ";C:\Program Files\Docker", [EnvironmentVariableTarget]::Machine)
 dockerd -D --bridge "none"
-#Remove-Item "$env:ProgramFiles\docker" -Recurse -Force -Verbose
+Remove-Item "$env:ProgramFiles\docker" -Recurse -Force -Verbose
