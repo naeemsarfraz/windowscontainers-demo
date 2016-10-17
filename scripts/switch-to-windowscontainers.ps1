9464fe48d33e0e1ee9c868448465e1254dd2dcf5
Naeem Sarfraz <naeem.sarfraz@gmail.com>
2016-10-18 00:39:44 +0100

    the proper way

--- scripts/switch-to-windowscontainers.ps1
+++ scripts/switch-to-windowscontainers.ps1
@@ -2,4 +2,5 @@ Invoke-WebRequest "https://master.dockerproject.org/windows/amd64/docker-1.13.0-
 Expand-Archive -Path "$env:TEMP\docker-1.13.0-dev.zip" -DestinationPath $env:ProgramFiles -Force
 $env:path += ";c:\program files\docker"
 [Environment]::SetEnvironmentVariable("Path", $env:Path + ";C:\Program Files\Docker", [EnvironmentVariableTarget]::Machine)
-dockerd -D
+dockerd --register-service
+Start-Service Docker
